9b938f830e415f84f5365be6e60d229295d107e7
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2022-05-27 17:33:27 +0100

    Initial specs and docs

---
+++ README.md
@@ -0,0 +1,41 @@
+# jeiss-specs
+
+Specifications, in TSV format, of the metadata in the header of `.dat` files produced by "Jeiss" Focused Ion Beam Scanning Electron Microscopes.
+
+## General notes
+
+- Arrays are in column-major ("Fortran") order.
+- Multi-byte data is in big-endian byte order, little-endian bit order.
+- Date is stored as `DD/MM/YYYY`, for some reason.
+- `specs/v0.tsv` is not a spec in its own right, but a core metadata common across all versions. In some cases, these fields determine the size of later metadata fields, so it's convenient to parse this first.
+- The image data begins at offset 1024, and has shape (`ChanNum`, `XResolution`, `YResolution`).
+- There may be a binary footer of indeterminate length, after the image data.
+
+## dtype
+
+Field data types are encoded as 3+ characters:
+
+- `>` for big-endian or `<` for little-endian byte order. All data types here are `>`, but it is included to be explicit. Note this is irrelevant for single-byte types (`S` and `u1`), but included anyway.
+- A letter for the data type: `f`loat, `i`nteger, `u`nsigned integer, `S`tring (ASCII, right-padded with null bytes).
+- An integer for how long the type is, in bytes.
+
+This representation is based on the NumPy [Array Interface](https://numpy.org/doc/stable/reference/arrays.interface.html#object.__array_interface__)'s typestrs.
+
+Some `>u1` fields represent booleans, where 0 is False and anything else is True.
+
+### Enums
+
+Some integer fields represent enums.
+`enums/${FIELD_NAME}.tsv` enumerate the integer values and the enums they represent.
+
+## shape
+
+`0` represents a single element (where `1` would represent a 1-length array).
+For multidimensional arrays, the length of each dimension is separated by a comma.
+In some versions, the shape depends on the value of an earlier field: in that case, the name of the field is used instead of an integer.
+
+## Example files
+
+For testing purposes, `example_files.tsv` lists publicly-accessible URLs to `.dat` files for specific versions.
+
+The TSV lists the version number, the hex digest of the MD5 hashsum of the file, and a public URL to the file.
---
+++ tsvfmt.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+from asyncio.log import logger
+from argparse import ArgumentParser
+from pathlib import Path
+from difflib import unified_diff
+import sys
+import logging
+from collections import defaultdict
+
+logger = logging.getLogger(__name__)
+
+
+def fmt_lines(orig_lines, fname=None):
+    out_lines = []
+    for line in orig_lines:
+        out_lines.append("\t".join(s.strip() for s in line.strip().split("\t")) + "\n")
+    out_lines.append("\n")
+    prefix = "" if not fname else "fname:"
+    diff = list(unified_diff(orig_lines, out_lines, prefix+"original", prefix+"formatted"))
+    return out_lines, diff
+
+
+def fmt_tsv(fpath: Path, write=False):
+    orig_lines = fpath.read_text().splitlines(keepends=True)
+    out_lines, diff = fmt_lines(orig_lines, str(fpath))
+
+    if write:
+        fpath.write_text("".join(out_lines))
+
+    return diff
+
+
+def fmt_stdin(write=False):
+    orig_lines = sys.stdin.readlines()
+    out_lines, diff = fmt_lines(orig_lines, "-")
+
+    if write:
+        sys.stdout.writelines(out_lines)
+
+    return diff
+
+
+def expand_args(args):
+    out_args = defaultdict(lambda: 0)
+    for arg in args:
+        if arg == "-":
+            args.append(None)
+            continue
+        p = Path(arg)
+        if not p.exists():
+            raise FileNotFoundError(f"Path not found: {p}")
+        elif p.is_file():
+            out_args[p] += 1
+        elif p.is_dir():
+            for p2 in p.glob("**/*.tsv"):
+                out_args[p2] += 1
+    return list(out_args)
+
+
+def main(args=None):
+    parser = ArgumentParser()
+    parser.add_argument("path", nargs="*", help="Path to a TSV file, directory (will be searched recursively), or - for stdin")
+
+    parser.add_argument("-c", "--check", action="store_true", help="Do not write out changes, but error if any changes would be made")
+    parsed = parser.parse_args(args)
+
+    sys.exit(_main(parsed.paths, parsed.check))
+
+
+def _main(paths, check):
+    all_paths = expand_args(paths)
+    if not all_paths:
+        logger.info("No paths given, nothing to do")
+        return 0
+
+    diffs = []
+    for path in all_paths:
+        if path is None:
+            diff = fmt_stdin(not check)
+        else:
+            diff = fmt_tsv(path, not check)
+        if diff:
+            logger.info("Format %s", path)
+        diffs.extend(diff)
+
+    if check and diffs:
+        logger.warning("File(s) not formatted")
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
